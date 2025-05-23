2ba0df197ff17e3f4a8597cdc84d043cd95ab0c3
junihoj <ezeonyekachukwu98@gmail.com>
2025-05-23 09:44:17 +0100

    edited github workflow to use dist instead of out

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV == 'production'
 const nextConfig = {
+    // basePath: isProd? '/onyekachukwu': '',
     output:"export",
     distDir: 'dist',
     images:{
